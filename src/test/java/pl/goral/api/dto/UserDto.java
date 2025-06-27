f388bcdeb06cdb452b9e65cf2a119f161eaa1df5
Tomasz Gorski <t.gorski@posnet.com>
2025-06-27 11:49:18 +0200

    user generator and dto fix

--- src/test/java/pl/goral/api/dto/UserDto.java
+++ src/test/java/pl/goral/api/dto/UserDto.java
@@ -1,12 +1,24 @@
 package pl.goral.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class UserDto {
+    @JsonProperty("first_name")
     private String username;
+
+    @JsonProperty("password")
     private String password;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("job")
+    private String job;
+    
+    private String avatar;
 }
